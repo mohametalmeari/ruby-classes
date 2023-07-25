343ba65d93b3269ab23c3aba88cf8b944cb59053
mohametalmeari <mohamet.almeari@gmail.com>
2023-07-25 12:54:02 +0300

    Create foods classes

--- animal.rb
+++ animal.rb
@@ -1,3 +1,6 @@
+require "./remover.rb"
+require "./foods.rb"
+
 class Animal
     # def name
     #     @name
@@ -19,10 +22,20 @@ class Animal
     @name = name
     @number_of_legs = number_of_legs
     @type = type
+    @liked_food = NoFood.new()
   end
 
   def speak
     "grrrr"
+  end
+
+  def remove_leg
+    remover = Remover.new()
+    @number_of_legs = remover.decrease(@number_of_legs)
+  end
+
+  def likes_food?(food)
+    @liked_food.is_liked?(food)
   end
 end
 
